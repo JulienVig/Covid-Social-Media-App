f6c3a74c51235ebdc24c8ce78b668f54c8b8e4ef
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-03 16:22:07 +0200

    Désappropriation de l'appli

--- back/src/main/java/coronhackathon/backend/controller/mainController.java
+++ back/src/main/java/coronhackathon/backend/controller/mainController.java
@@ -2,12 +2,13 @@ package coronhackathon.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class mainController {
     @GetMapping("/")
-    public String hello(){
-        return "Vous êtes bien sur l'application Spring de Ben";
+    public String hello(@RequestParam(defaultValue = "Moi") String name){ //Ajouter ?name=Votreprenom à la fin de l'URL
+        return "Vous êtes bien sur l'application Spring de "+name;
     }
 }
