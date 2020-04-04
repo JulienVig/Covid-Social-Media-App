7ee5523256b1c370207a8209d397447ca61ee13b
Elisa Michelet <elisa.michelet@epfl.ch>
2020-04-04 19:04:02 +0200

    Entity Tag créée + relationships

--- back/src/main/java/coronhackathon/backend/entity/Tag.java
+++ back/src/main/java/coronhackathon/backend/entity/Tag.java
@@ -1,4 +1,36 @@
 package coronhackathon.backend.entity;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "isA")
+    Set<User> challenges;
+
+    /* ----getters and setters---- */
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- back/src/main/java/coronhackathon/backend/repository/TagRepository.java
+++ back/src/main/java/coronhackathon/backend/repository/TagRepository.java
@@ -1,4 +1,14 @@
 package coronhackathon.backend.repository;
 
-public interface TagRepository {
+
+import coronhackathon.backend.entity.Tag;
+import coronhackathon.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    public Optional<Tag> getTagById(long id);
+    public Optional<Tag> getTagByName(String name);
 }
