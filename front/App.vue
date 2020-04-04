55f475e8ce6c15ab8ab312e512d50f49721698ee
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-04 10:45:24 +0200

    It works it works it works

--- front/App.vue
+++ front/App.vue
@@ -1,7 +1,18 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-    <my-login/>
+  <!--
+    Now we provide each todo-item with the todo object
+    it's representing, so that its content can be dynamic.
+    We also need to provide each component with a "key",
+    which will be explained later.
+  -->
+    <todo-item
+      class="text-container"
+      v-for="todo in todos"
+      :key="todo.text"
+      :item="todo"
+    />
+    <text-input>It happened !!</text-input>
   </view>
 </template>
 
@@ -12,17 +23,31 @@
   justify-content: center;
   flex: 1;
 }
-.text-color-primary {
+.text-container {
   color: blue;
+  padding: 2;
+  font-size: 22;
+}
+.text-input-container {
+  width: 300;
+  height: 40;
+  font-size: 22;
+  border-color: gray;
 }
 </style>
 
 <script>
-import Login from "./login_page.vue";
+import TodoItem from "./todoItem";
 export default {
-  name: 'login',
-  components: {
-    'my-login': Login
+  components: { TodoItem },
+  data: function() {
+    return {
+      todos: [
+        { id: 1, text: "Learn JavaScript" },
+        { id: 2, text: "Learn Vue" },
+        { id: 3, text: "Build something awesome" }
+      ]
+    };
   }
-}
+};
 </script>
