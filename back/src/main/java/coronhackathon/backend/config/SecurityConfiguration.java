1fb9f3259e25605973b655104cd961d155d0fde4
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-04 15:57:47 +0200

    Changed backend config to test login

--- back/src/main/java/coronhackathon/backend/config/SecurityConfiguration.java
+++ back/src/main/java/coronhackathon/backend/config/SecurityConfiguration.java
@@ -1,11 +1,23 @@
 package coronhackathon.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("userPass")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+    }
 
     @Override
     /**
@@ -15,7 +27,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().
-                authorizeRequests().antMatchers("/**").permitAll();
+                authorizeRequests().antMatchers("/api/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
