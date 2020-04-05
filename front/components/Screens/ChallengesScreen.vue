724e8f074dfca221aebb59cd5d6ec06ce383bd69
Julien <julien.vignoud@epfl.ch>
2020-04-05 09:05:00 +0200

    Working camera roll  and camera

--- front/components/Screens/ChallengesScreen.vue
+++ front/components/Screens/ChallengesScreen.vue
@@ -2,9 +2,60 @@
   <view class="container">
     <text class="heading">Challenge</text>
     <text>This is the Challenge screen</text>
+    <button
+    :on-press="() => pickImage()"
+    title="Pick an image from camera roll"
+    />
+    <button
+    :on-press="() => takePhoto()"
+    title="Take a photo"
+    />
   </view>
 </template>
 
+<script>
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+export default {
+    methods:{
+        pickImage : async () => {
+            const {
+              status: cameraRollPerm
+            } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+            // only if user allows permission to camera roll
+            if (cameraRollPerm === 'granted') {
+              let pickerResult = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+              });
+
+              // handleImagePicked(pickerResult);
+            }
+        },
+        takePhoto : async () => {
+           const {
+             status: cameraPerm
+           } = await Permissions.askAsync(Permissions.CAMERA);
+
+           const {
+             status: cameraRollPerm
+           } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+           // only if user allows permission to camera AND camera roll
+           if (cameraPerm === 'granted' && cameraRollPerm === 'granted') {
+             let pickerResult = await ImagePicker.launchCameraAsync({
+               allowsEditing: true,
+               aspect: [4, 3],
+             });
+
+             // handleImagePicked(pickerResult);
+           }
+       },
+    }
+}
+</script>
 <style>
 .container {
   align-items: center;
