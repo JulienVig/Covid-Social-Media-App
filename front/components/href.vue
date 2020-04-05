0ef5f3176a883d45cd284ce8f0dc6ce82be518f2
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-05 22:31:47 +0200

    ON A UN HREF MAISON

---
+++ front/components/href.vue
@@ -0,0 +1,33 @@
+<template>
+    <touchable-opacity class = "link" :on-press="() => click()">
+        <text class= "link">{{displayedLink}}</text>
+    </touchable-opacity>
+</template>
+
+<style>
+    .link {
+        color: blue;
+    }
+</style>
+
+<script>
+import {Linking} from "react-native"
+export default {
+  props: ['url', 'name'],
+  data: function() {
+    return {
+      displayedLink: (this.name === undefined) ? this.url : this.name
+    }
+  },
+  methods: {
+    click () {
+      Linking.openURL(this.url)
+    },
+  },
+  computed: {
+    computeDisplay() {
+      displayedLink = (name === null) ? url : name
+    }
+  }
+};
+</script>
