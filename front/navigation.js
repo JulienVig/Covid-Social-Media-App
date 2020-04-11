e34ca6207196b1c5488540089ce078cb791742f7
Julien <julien.vignoud@epfl.ch>
2020-04-11 22:58:53 +0200

    Remove useless code in navigation.js

--- front/navigation.js
+++ front/navigation.js
@@ -21,9 +21,6 @@ const CategoryTab = createStackNavigator(
         ChallengeDetail : ChallengeDetailScreen,
         Validation : ValidationChallengeScreen,
     },
-    {
-      transitionConfig
-    }
 )
 
 const ChallengesTab = createStackNavigator(
@@ -117,33 +114,6 @@ const styles = StyleSheet.create({
   }
 })
 
-const transitionConfig = () => {
-  console.log('transitionConfig')
-  return {
-    transitionSpec: {
-      duration: 500,
-      easing: Easing.out(Easing.poly(4)),
-      timing: Animated.timing,
-      useNativeDriver: true,
-    },
-    screenInterpolator: sceneProps => {
-      const { layout, position, scene } = sceneProps;
- 
-      const thisSceneIndex = scene.index;
-      const width = layout.initWidth;
- 
-      const translateX = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex],
-        outputRange: [-width, 0],
-        extrapolate: 'clamp'
-      });
- 
-      return {
-        transform: [{ translateX }]
-      }
-    }
-  }
-}
  
 
 export default createAppContainer(StackNavigator)
