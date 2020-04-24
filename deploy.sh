7658f5099d5da2a1fb37a7e97e5503c251891b61
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 15:48:48 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+ 
+# Variables
+remoteUserName="ec2-user"
+remoteServerIp=$2
+remoteServerKey=$1
+
+# Script
+echo "Starting script"
+echo remoteServerIp
+echo remoteServerKey
+ 
+# Scp to instance
+
+exit 0
