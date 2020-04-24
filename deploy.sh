dce87cfd14972acb8345857848660a40129d6390
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-25 00:47:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "Starting deploy script"
 cd ..
 chmod 400 amazonkey-coronhackathon.pem
 
-scp -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i amazonkey-coronhackathon.pem /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
-ssh -i "amazonkey-coronhackathon.pem" ec2-user@ec2-18-234-142-249.compute-1.amazonaws.com < remote-script.sh
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "amazonkey-coronhackathon.pem" /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "amazonkey-coronhackathon.pem" ec2-user@ec2-18-234-142-249.compute-1.amazonaws.com < remote-script.sh
 
 exit 0
