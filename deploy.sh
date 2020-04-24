7d59e6bd43818776a118cde4c91949586f7f1002
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 16:36:51 +0200

    Please work

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
  
-# Variables
-remoteUserName="ec2-user"
-remoteServerIp=$1
+#$1 is the instance IP
+#$2 is the private key
 
 # Script
-echo "Starting script"
-echo $remoteServerIP
-echo "$remoteServerIP"
-echo $remoteUserName
-echo args:
-echo $1
-echo $2
+echo "Starting deploy script"
  
 # Scp to instance
+
+scp /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home
 
 exit 0
