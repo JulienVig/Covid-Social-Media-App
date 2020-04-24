ae343afe8c913656af98aafbe120ba2647450c71
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 23:38:36 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,7 @@
 # Script
 echo "Starting deploy script"
 
-#Preparing the key file
-echo $2 > key.pem
-chmod 400 key.pem
-ssh-keygen -f key.pem -p
-
 # Scp to instance
-echo "printing the key"
-echo $2
-scp -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
+scp -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i amazonkey-coronhackathon.pem /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
 
 exit 0
