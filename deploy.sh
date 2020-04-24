614a614bbda1bdacaaac017e59278da213f05cb7
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 17:51:45 +0200

    Typo in chmod

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "Starting deploy script"
 
 #Preparing the key file
 echo $2 > key.pem
-chmod 400 key.pm
+chmod 400 key.pem
 
 # Scp to instance
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
