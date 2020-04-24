7ceb538165e0ecf17af9a81a292bce46d64ff755
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 23:59:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Starting deploy script"
 
 # Scp to instance
 cd ..
+chmod 400 amazonkey-coronhackathon.pem
 scp -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i amazonkey-coronhackathon.pem /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home/ec2-user
 
 exit 0
