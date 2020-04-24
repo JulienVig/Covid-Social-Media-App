f01596ceaea8f9f1c38a2b7f92e5c250725688ca
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-24 16:40:50 +0200

    Ignore confirmation

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ echo "Starting deploy script"
  
 # Scp to instance
 
-scp /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar ec2-user@18.234.142.249:/home
+scp -o /home/travis/build/JulienB-Epfl/Coronhackathon/back/build/libs/backend-0.0.1-SNAPSHOT.jar -o ec2-user@18.234.142.249:/home
 
 exit 0
