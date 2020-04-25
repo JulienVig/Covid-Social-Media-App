8cc0b672ac964fc384e3fd1e34c8b92fcf68e0e1
JulienB-Epfl <33035980+JulienB-Epfl@users.noreply.github.com>
2020-04-25 14:18:36 +0200

    Some comments

--- remote-script.sh
+++ remote-script.sh
@@ -8,8 +8,9 @@ kill $PID
 #Replacing the old file by the new one freshly transfered
 rm backend.jar
 mv backend-bis.jar backend.jar
-#Launching new server
+#Launching new server in background
 nohup java -jar backend.jar > log.txt 2> errors.txt < /dev/null &
+#Remembering the PID to kill it latter
 PID=$!
 echo $PID > pid.txt
 exit 0
